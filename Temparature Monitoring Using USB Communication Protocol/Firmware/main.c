d3738b51cda80fb06d526943231ac91fc9f2c84d
mehran memarnejad <memarnejadm74@gmail.com>
2017-06-18 17:47:51 +0430

    ADC added and Application layer protocol modified

--- Temparature Monitoring Using USB Communication Protocol/Firmware/main.c
+++ Temparature Monitoring Using USB Communication Protocol/Firmware/main.c
@@ -1,11 +1,22 @@
 //Be nam _e_ khoda
+
 /**
- * Project: AVR ATtiny USB Tutorial at http://codeandlife.com/
- * Author: Joonas Pihlajamaa, joonas.pihlajamaa@iki.fi
- * Inspired by V-USB example code by Christian Starkjohann
- * Copyright: (C) 2012 by Joonas Pihlajamaa
- * License: GNU GPL v3 (see License.txt)
+ * Temparature Monitoring Using USB Protocol
+ * Interface Ciruit Course, Iran University of Science and Technology (IUST)
+ * Author : Mehran Memarnejad (92521267)
+ *
+ *
+ * Acknowledgment:
+ *
+ * To Accomplish this project, I have used some resource.
+ * The Main resources are :
+ * 		Project: AVR ATtiny USB Tutorial at http://codeandlife.com/
+ * 		Author: Joonas Pihlajamaa, joonas.pihlajamaa@iki.fi
+ * 		Inspired by V-USB example code by Christian Starkjohann
+ * 		Copyright: (C) 2012 by Joonas Pihlajamaa
+ * 		License: GNU GPL v3 (see License.txt)
  */
+ 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
@@ -15,13 +26,25 @@
 #define F_CPU 12000000L
 #include <util/delay.h>
 
-#define USB_LED_OFF 0
-#define USB_LED_ON  1
-#define USB_DATA_OUT 2
-#define USB_DATA_WRITE 3
-#define USB_DATA_IN 4
+#define FAN_PORT PORTB
+#define FAN_DDR DDRB
 
-static uchar replyBuf[16] = "Be nam_e_ khoda";
+#define STUDENT 1
+#define TEACHER 0
+
+//Our Application Layer Protocol
+#define USB_GET_TEMP 0
+#define USB_FAN_ON  1
+#define USB_FAN_OFF 2
+#define USB_GET_STUDENT 3
+#define USB_SET_STUDENT 4
+#define USB_GET_TEACHER 5
+#define USB_SET_TEACHER 6
+
+static uchar student[18] = "Mehran Memarnejad";
+static uchar teacher[12] = "DR.Patooghi";
+static uchar whichString;
+static uchar temparature;
 static uchar dataReceived = 0, dataLength = 0; // for USB_DATA_IN
 
 // this gets called when custom control message is received
@@ -29,29 +52,48 @@ USB_PUBLIC uchar usbFunctionSetup(uchar data[8]) {
 	usbRequest_t *rq = (void *)data; // cast data to correct type
 	
 	switch(rq->bRequest) { // custom command is in the bRequest field
-    case USB_LED_ON:
-		PORTB |= 1; // turn LED on
-		return 0;
-	case USB_LED_OFF: 
-		PORTB &= ~1; // turn LED off
-		return 0;
-    case USB_DATA_OUT: // send data to PC
-        usbMsgPtr = replyBuf;
-        return sizeof(replyBuf);
-	case USB_DATA_WRITE: // modify reply buffer
-		replyBuf[7] = rq->wValue.bytes[0];
-		replyBuf[8] = rq->wValue.bytes[1];
-		replyBuf[9] = rq->wIndex.bytes[0];
-		replyBuf[10] = rq->wIndex.bytes[1];
-		return 0;
-    case USB_DATA_IN: // receive data from PC
-		dataLength  = (uchar)rq->wLength.word;
-        dataReceived = 0;
-		
-		if(dataLength  > sizeof(replyBuf)) // limit to buffer size
-			dataLength  = sizeof(replyBuf);
+	
+		case USB_GET_TEMP:
+			usbMsgPtr = &temparature; // send Temparature to PC
+			return sizeof(temparature);
 			
-		return USB_NO_MSG; // usbFunctionWrite will be called now
+		case USB_FAN_ON:
+			FAN_PORT |= 1; // turn FAN on
+			return 0;
+			
+		case USB_FAN_OFF: 
+			FAN_PORT &= ~1; // turn FAN off
+			return 0;
+			
+		case USB_GET_STUDENT: // send data to PC
+			usbMsgPtr = student;
+			return sizeof(student);
+			
+		case USB_SET_STUDENT: // receive data from PC
+			dataLength  = (uchar)rq->wLength.word;
+			dataReceived = 0;
+			
+			if(dataLength  > sizeof(student)){ // limit to buffer size
+				dataLength  = sizeof(student);
+				whichString = STUDENT;
+			}
+	
+			return USB_NO_MSG; // usbFunctionWrite will be called now
+			
+		case USB_GET_TEACHER: // send data to PC
+			usbMsgPtr = teacher;
+			return sizeof(teacher);
+			
+		case USB_SET_TEACHER: // receive data from PC
+			dataLength  = (uchar)rq->wLength.word;
+			dataReceived = 0;
+			
+			if(dataLength  > sizeof(teacher)){// limit to buffer size
+				dataLength  = sizeof(teacher);
+				whichString = TEACHER;
+			}
+			
+			return USB_NO_MSG; // usbFunctionWrite will be called now
     }
 
     return 0; // should not get here
@@ -60,20 +102,46 @@ USB_PUBLIC uchar usbFunctionSetup(uchar data[8]) {
 // This gets called when data is sent from PC to the device
 USB_PUBLIC uchar usbFunctionWrite(uchar *data, uchar len) {
 	uchar i;
-			
-	for(i = 0; dataReceived < dataLength && i < len; i++, dataReceived++)
-		replyBuf[dataReceived] = data[i];
+	
+	if(whichString == STUDENT){
+		for(i = 0; dataReceived < dataLength && i < len; i++, dataReceived++)
+			student[dataReceived] = data[i];
+	}
+	
+	else{
+		for(i = 0; dataReceived < dataLength && i < len; i++, dataReceived++)
+			teacher[dataReceived] = data[i];
+	}
 		
-    return (dataReceived == dataLength); // 1 if we received it all, 0 if not
+	return (dataReceived == dataLength); // 1 if we received it all, 0 if not
 }
 
-int main() {
-	uchar i;
+//This Function configure ADC
+void ADCInit(void){
+	
+	ADCSRA = 0x87;
+	ADMUX = 0xE0;
 
-	DDRB = 1; // PB0 as output
+}
+
+//This Function Calculates temparature
+void getTemparature(uchar *temparature){
+	
+	ADCSRA |= (1<<ADSC);
+	while((ADCSRA & (1<<ADIF)) == 0);
+	*temparature = ADCH;
+	
+}
+
+
+int main() {
 	
     wdt_enable(WDTO_1S); // enable 1s watchdog timer
-
+	
+	FAN_DDR = 1;
+	
+	ADCInit();
+	
     usbInit();
 	
     usbDeviceDisconnect(); // enforce re-enumeration
@@ -87,6 +155,7 @@ int main() {
 	
     while(1) {
         wdt_reset(); // keep the watchdog happy
+		getTemparature(&temparature);
         usbPoll();
     }
 	
