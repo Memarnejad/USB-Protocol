c40eb82bde2dd4badb9dfe1554a33f900c2210f9
mehran memarnejad <memarnejadm74@gmail.com>
2017-06-21 12:51:14 +0430

    usbtest.c modified to match the temparature project(NOT TESTED YET)

--- Temparature Monitoring Using USB Communication Protocol/Firmware/usbtest.c
+++ Temparature Monitoring Using USB Communication Protocol/Firmware/usbtest.c
@@ -1,10 +1,23 @@
+//Be nam _e_ khoda
+
 /**
- * Project: AVR ATtiny USB Tutorial at http://codeandlife.com/
- * Author: Joonas Pihlajamaa, joonas.pihlajamaa@iki.fi
- * Based on V-USB example code by Christian Starkjohann
- * Copyright: (C) 2012 by Joonas Pihlajamaa
- * License: GNU GPL v3 (see License.txt)
+ * Temparature Monitoring Using USB Protocol
+ * Interface Ciruit Course, Iran University of Science and Technology (IUST)
+ * Author : Mehran Memarnejad (92521267)
+ *
+ *
+ * Acknowledgment:
+ *
+ * To Accomplish this project, I have used some resource.
+ * The Main resources are :
+ * 		Project: AVR ATtiny USB Tutorial at http://codeandlife.com/
+ * 		Author: Joonas Pihlajamaa, joonas.pihlajamaa@iki.fi
+ * 		Inspired by V-USB example code by Christian Starkjohann
+ * 		Copyright: (C) 2012 by Joonas Pihlajamaa
+ * 		License: GNU GPL v3 (see License.txt)
  */
+ 
+ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,11 +26,13 @@
 #include <usb.h>
 
 // same as in main.c
-#define USB_LED_OFF 0
-#define USB_LED_ON  1
-#define USB_DATA_OUT 2
-#define USB_DATA_WRITE 3
-#define USB_DATA_IN 4
+#define USB_GET_TEMP 0
+#define USB_FAN_ON  1
+#define USB_FAN_OFF 2
+#define USB_GET_STUDENT 3
+#define USB_SET_STUDENT 4
+#define USB_GET_TEACHER 5
+#define USB_SET_TEACHER 6
 
 // used to get descriptor strings for device identification 
 static int usbGetDescriptorString(usb_dev_handle *dev, int index, int langid, 
@@ -120,11 +135,13 @@ int main(int argc, char **argv) {
 
 	if(argc < 2) {
 		printf("Usage:\n");
-		printf("usbtext.exe on\n");
-		printf("usbtext.exe off\n");
-		printf("usbtext.exe out\n");
-		printf("usbtext.exe write\n");
-		printf("usbtext.exe in <string>\n");
+		printf("usbtext.exe getTemp\n");
+		printf("usbtext.exe fanOn\n");
+		printf("usbtext.exe fanOff\n");
+		printf("usbtext.exe getStudent\n");
+		printf("usbtext.exe setStudent\n");
+		printf("usbtext.exe getTeacher\n");
+		printf("usbtext.exe setTeacher\n");
 		exit(1);
 	}
 	
@@ -135,28 +152,37 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
-	if(strcmp(argv[1], "on") == 0) {
+	if(strcmp(argv[1], "getTemp") == 0) { //on
 		nBytes = usb_control_msg(handle, 
             USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, 
-			USB_LED_ON, 0, 0, (char *)buffer, sizeof(buffer), 5000);
-	} else if(strcmp(argv[1], "off") == 0) {
+			USB_GET_TEMP, 0, 0, (char *)buffer, sizeof(buffer), 5000);
+        printf("Got %d bytes ... Temparature : %s\n", nBytes, buffer);
+	} else if(strcmp(argv[1], "fanOn") == 0) { //off
 		nBytes = usb_control_msg(handle, 
             USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, 
-			USB_LED_OFF, 0, 0, (char *)buffer, sizeof(buffer), 5000);
-	} else if(strcmp(argv[1], "out") == 0) {
+			USB_FAN_ON, 0, 0, (char *)buffer, sizeof(buffer), 5000);
+	} else if(strcmp(argv[1], "fanOff") == 0) { //out
 		nBytes = usb_control_msg(handle, 
             USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, 
-			USB_DATA_OUT, 0, 0, (char *)buffer, sizeof(buffer), 5000);
-        printf("Got %d bytes: %s\n", nBytes, buffer);
-	} else if(strcmp(argv[1], "write") == 0) {
+			USB_FAN_OFF, 0, 0, (char *)buffer, sizeof(buffer), 5000);
+	} else if(strcmp(argv[1], "getStudent") == 0) {  //write
 		nBytes = usb_control_msg(handle, 
             USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, 
-			USB_DATA_WRITE, 'T' + ('E' << 8), 'S' + ('T' << 8), 
-            (char *)buffer, sizeof(buffer), 5000);
-	} else if(strcmp(argv[1], "in") == 0 && argc > 2) {
+			USB_GET_STUDENT, 0, 0, (char *)buffer, sizeof(buffer), 5000);
+        printf("Got %d bytes ... Student : %s\n", nBytes, buffer);
+	} else if(strcmp(argv[1], "setStudent") == 0 && argc > 2) { //in
 		nBytes = usb_control_msg(handle, 
             USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_OUT, 
-			USB_DATA_IN, 0, 0, argv[2], strlen(argv[2])+1, 5000);
+			USB_SET_STUDENT, 0, 0, argv[2], strlen(argv[2])+1, 5000);
+	} else if(strcmp(argv[1], "getTeacher") == 0) {  //write
+		nBytes = usb_control_msg(handle, 
+            USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, 
+			USB_GET_TEACHER, 0, 0, (char *)buffer, sizeof(buffer), 5000);
+        printf("Got %d bytes ... Teacher : %s\n", nBytes, buffer);
+	} else if(strcmp(argv[1], "setTeacher") == 0 && argc > 2) { //in
+		nBytes = usb_control_msg(handle, 
+            USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_OUT, 
+			USB_SET_TEACHER, 0, 0, argv[2], strlen(argv[2])+1, 5000);
 	}
 	
 	if(nBytes < 0)
